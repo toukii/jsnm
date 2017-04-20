ab223ce126014ded195433b5c81d4468f9312d8a
时永宾 <ybshi1@chinaums.com>
2017-04-20 14:07:26 +0800

    fmt

--- jsnm.go
+++ jsnm.go
@@ -1,8 +1,8 @@
 package jsnm
 
 import (
-	"regexp"
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -147,21 +147,21 @@ func (j *Jsnm) ArrLoc(i int) *Jsnm {
 	return NewJsnm(arr[i])
 }
 
-func (j *Jsnm) ArrGet(path ...string) *Jsnm{
-	if len(path)<=0 {
+func (j *Jsnm) ArrGet(path ...string) *Jsnm {
+	if len(path) <= 0 {
 		return j
 	}
-	fmt.Println(path)
-	if strings.HasSuffix(path[0],"\"") {
-		path_0:=strings.Trim(path[0],"\"")
+	// fmt.Println(path)
+	if strings.HasSuffix(path[0], "\"") {
+		path_0 := strings.Trim(path[0], "\"")
 		return j.Get(path_0).ArrGet(path[1:]...)
 	}
-	if matched,err:=regexp.MatchString("\\d", path[0]); matched {
-		if err!=nil {
-			fmt.Println("path-0",err)
+	if matched, err := regexp.MatchString("\\d", path[0]); matched {
+		if err != nil {
+			fmt.Println("path-0", err)
 		}
-		loc,err2:=strconv.ParseInt(path[0],10,64)
-		if err2 !=nil {
+		loc, err2 := strconv.ParseInt(path[0], 10, 64)
+		if err2 != nil {
 			fmt.Println(err2)
 		}
 		return j.ArrLoc(int(loc)).ArrGet(path[1:]...)
