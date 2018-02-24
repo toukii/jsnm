38735b07ca230a0f06706d5b4e6a49b9b1790c34
shiyongbin <shiyongbin@ezbuy.com>
2018-02-24 18:17:33 +0800

    range-break

---
+++ go.mod
@@ -0,0 +1,6 @@
+module "github.com/toukii/jsnm"
+
+require (
+"github.com/everfore/exc" vgo.0.1
+	"github.com/toukii/goutils" v0.1.1
+)
--- range.go
+++ range.go
@@ -1,9 +1,11 @@
 package jsnm
 
-func (j *Jsnm) Range(do func(i int, ji *Jsnm)) *Jsnm {
+func (j *Jsnm) Range(do func(i int, ji *Jsnm) bool) *Jsnm {
 	arr := j.Arr()
 	for idx, it := range arr {
-		do(idx, it)
+		if do(idx, it) {
+			break
+		}
 	}
 	return j
 }
